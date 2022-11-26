2f8beb8042879e55ecb53d3eec6716356126ec65
Rajesh Kumaravel <rajesh.kumaravel@tarento.com>
2022-11-26 17:39:29 +0530

    Added list and details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormListComponent } from './modules/form-list/form-list.component';
+import { FormDetailsComponent } from './modules/form-details/form-details.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: 'form-list', pathMatch: 'full' },
+  { path: 'form-list', component: FormListComponent, pathMatch: 'full' },
+  { path: 'form-details', component: FormDetailsComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: 'form-list' }
+];
 
 @NgModule({
+  declarations: [
+    FormListComponent,
+    FormDetailsComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
