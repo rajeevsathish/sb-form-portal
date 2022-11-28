d8f9207f04f97c1ac3c2218d106ba3aca9e4ed4c
Rajesh Kumaravel <rajesh.kumaravel@tarento.com>
2022-11-28 09:52:06 +0530

    Initial setup

--- src/app/modules/form-list/form-list.component.ts
+++ src/app/modules/form-list/form-list.component.ts
@@ -12,7 +12,7 @@ export class FormListComponent implements OnInit {
 
   ngOnInit(): void {
     this.cities = [
-      { name: 'New York', code: 'NY' },
+      { name: 'New York City Name', code: 'NY' },
       { name: 'Rome', code: 'RM' },
       { name: 'London', code: 'LDN' },
       { name: 'Istanbul', code: 'IST' },
